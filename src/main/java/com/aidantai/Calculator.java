c30f68f3bdd6c7e245c9deba291165ef8d32424e
aidantai <aidantai100@gmail.com>
2019-10-29 22:32:18 -0700

    Creation of the beginning of the program
    U know, the welcome part

---
+++ src/main/java/com/aidantai/Calculator.java
@@ -0,0 +1,39 @@
+package com.aidantai;
+
+import java.util.*;
+
+public final class Calculator {
+    private Calculator() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        boolean kinematics = false;
+
+        System.out.println("Welcome to the Physics Calculator!");
+        while(true) { // Determines if the problem is kinematics or projectile motion. Beware of while(true)
+            System.out.println("Are you trying to solve a kinematics or projectiles problem?");
+            String kinproj = keyboard.next().toLowerCase();
+            if (kinproj.equals("kinematics")) {
+                kinematics = true;
+                break;
+            } else if (kinproj.equals("projectiles")) {
+                kinematics = false;
+                break;
+            } else {
+                System.out.println("Sorry, that's not an option. Die.");
+                continue;
+            }
+        }
+
+        System.out.println("Enter the variable that you are trying to find: ");
+        String fVar = keyboard.next().toLowerCase();
+        
+        System.out.println(kinematics);
+        keyboard.close();
+    }
+}
